4067cc1b2dd068151e52b0bbff825c02926147b3
vyoung708 <vyoung708@yahoo.com>
2022-04-08 00:06:44 -0400

    add centering part

--- run.py
+++ run.py
@@ -9,28 +9,40 @@ def detectDistanceToTrash(moved_amount, orig_perim, new_perim):
     dist = moved_amount / (1-(orig_perim/new_perim))
     return dist + moved_amount
 
+def centerTrash(trash_center_x, image_center_x):
+    # turn robot right if trash is to the right of the image center ( trash center x is > than image center x )
+    if trash_center_x > image_center_x:
+        #robot.turn_right(trash_center_x - image_center_x) or something
+        pass
+    # turn robot left if trash is to the left ( trash center x is < than image center x )
+    elif trash_center_x < image_center_x:
+        #robot.turn_left(image_center_x - trash_center_x) or something
+        pass
+    # trash should now be centered
 
+# assumes we are getting a single frame from the camera as an image
 def doSteps():
+    # for now
+    image = None
     # this should return a bounding rectangle around the trash
-    trash_width, trash_height, trash_x, trash_y = detectTrash()
+    trash_width, trash_height, trash_x, _ = detectTrash()
 
     trash_center_x = trash_x + (trash_width / 2)
-    trash_center_y = trash_y + (trash_height / 2)
 
     trash_area = trash_width * trash_height
     trash_perim = (trash_width * 2) + (trash_height * 2)
 
-    # turn robot right if trash is to the right of the image center ( trash center x is > than image center x )
+    # assumes the image is an opencv image
+    _, width = image.shape[:2]
 
-    # turn robot left if trash is to the left ( trash center x is < than image center x )
+    image_center_x = width / 2
 
-    # trash should now be centered
+    centerTrash(trash_center_x, image_center_x)
 
     # move robot backward x amount 
     # (this does not need to be in pixels, should be in meters or inches) 
     # and calculate trash growth in image
     # should go backward so we do not run into the trash on accident
-
     move_backward = 0
 
     #robot.move_backward(move_backward)
